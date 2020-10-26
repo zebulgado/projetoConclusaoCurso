5b93eabb7e01ccb4dc961d084f78e2884f85a946
zebulgado <othon.negromonte@gmail.com>
2020-10-26 19:13:07 -0300

    Project Creation

---
+++ src/main/java/br/com/qualiti/projetoConclusaoCurso/ProjetoConclusaoCursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.qualiti.projetoConclusaoCurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoConclusaoCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoConclusaoCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/qualiti/projetoConclusaoCurso/ProjetoConclusaoCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.qualiti.projetoConclusaoCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoConclusaoCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
