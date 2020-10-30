d25110d6b02e01ec8966dc65e0db3b8f2490383c
Felipe Pontes <felipe92pontes@gmail.com>
2020-10-30 17:28:26 -0300

    Add repository guest

---
+++ src/main/java/br/com/qualiti/projetoConclusaoCurso/repository/GuestRepository.java
@@ -0,0 +1,11 @@
+package br.com.qualiti.projetoConclusaoCurso.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.qualiti.projetoConclusaoCurso.model.Guest;
+
+@Repository
+public interface GuestRepository extends JpaRepository<Guest, String>{
+
+}
